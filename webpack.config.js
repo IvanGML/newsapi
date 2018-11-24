bea9ebf2f88fd821e03cc487f1fadd4096992d9a
GOMEL\Ivan_Somau <ivan_somau@epam.com>
2018-11-24 16:03:52 +0300

    general structure changes implementation

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 let path = require('path');
 
 module.exports = {
-    context: path.resolve(__dirname, 'public/src/js'),
+    context: path.resolve(__dirname, 'src/js'),
     entry: './index.js',
     output: {
         filename: './bundle.js',
-        path: path.resolve(__dirname, 'public')
+        path: path.resolve(__dirname, 'src/dist')
     },
     mode: 'development',
     watch: true,

1d67205c9cb053d81f39f2b2f920c80a0c16fdca
IvanGML <somovlife@gmail.com>
2019-01-22 19:21:46 +0300

    add post and delete methods

--- express-app/app.js
+++ express-app/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const news = require('./data/news.json');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const path = require('path');
 const bodyParser = require('body-parser');
 
@@ -8,15 +8,17 @@ const app = express();
 
 app.use(bodyParser());
 
+const findById = (arr, id) => arr.find(item => item.id == id);
+
 app.engine('pug', require('pug').__express);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// third party loger
+// third party logger
 // app.use(morgan('dev'));
 
-// custom loger
+// custom logger
 app.use((req, res, next) => {
     let date = new Date(Date.now()).toLocaleString("en-US");
     console.log(`${date} === ${req.method} === ${req.url}`);
@@ -40,19 +42,40 @@ app.get('/news', (req, res) => {
 
 app
     .get('/news/:id', (req, res) => {
-        let newsList = news.find(item => item.id == req.params.id);
+        let newsList = findById(news, req.params.id);
         if(!newsList) return res.status(404).send(`
             <h1 style='text-align: center'>Page with id ${req.params.id} not found ¯\\_(ツ)_/¯ </h1>`
         );
         res.json(newsList);
     })
+    .post('/news/:id', (req, res) => {
+        let post = findById(news, req.params.id);
+        let updatedCustomer = req.body;
+        if(post != null) {
+            post = {...post, ...updatedCustomer};
+            console.log("--->Created Successfully, customers: \n" + JSON.stringify(post, null, 4))
+            res.end("Created Successfully! \n" + JSON.stringify(post, null, 4));
+        } else {
+            res.end("Don't Exist Customer:\n:" + JSON.stringify(updatedCustomer, null, 4));
+        }
+    })
     .put('/news/:id', (req, res) => {
-        let post = news.find(item => item.id == req.params.id);
+        let post = findById(news, req.params.id);
         let updatedCustomer = req.body;
         if(post != null) {
             post = {...post, ...updatedCustomer};
             console.log("--->Update Successfully, customers: \n" + JSON.stringify(post, null, 4))
             res.end("Update Successfully! \n" + JSON.stringify(post, null, 4));
+        } else {
+            res.end("Don't Exist Customer:\n:" + JSON.stringify(updatedCustomer, null, 4));
+        }
+    })
+    .delete('/news/:id', (req, res) => {
+        let post = findById(news, req.params.id);
+        if(post != null) {
+            // actions to delete 'post' from db
+            console.log("--->Deleted Successfully, customers: \n" + JSON.stringify(post, null, 4))
+            res.end("Deleted Successfully! \n" + JSON.stringify(post, null, 4));
         } else {
             res.end("Don't Exist Customer:\n:" + JSON.stringify(updatedCustomer, null, 4));
         }

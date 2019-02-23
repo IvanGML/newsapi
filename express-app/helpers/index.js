bf009b4c7f6218a7060d6e42040d13847a6c6018
IvanGML <somovlife@gmail.com>
2019-02-23 16:26:54 +0300

    [task-8]: update mongoose schemes regarding 'newsapi' response json structure

--- express-app/helpers/index.js
+++ express-app/helpers/index.js
@@ -1,12 +1,17 @@
 const findById = (arr, id) => arr.find(item => item.id == id);
 
 const newSchema = body => (new NewsPost({
-    name: body.name,
+    source: {
+        id: body.source.id,
+        name: body.source.name
+    },
+    author: body.author,
+    title: body.title,
     description: body.description,
     url: body.url,
-    category: body.category,
-    language: body.language,
-    country: body.country,
+    urlToImage: body.urlToImage,
+    publishedAt: body.publishedAt,
+    content: body.content,
 }));
 
 module.exports = {

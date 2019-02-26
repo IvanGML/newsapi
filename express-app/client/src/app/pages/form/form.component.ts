f0b14778b550dd40fb7adf76f46648f765ad9d93
IvanGML <somovlife@gmail.com>
2019-02-26 14:35:19 +0300

    [task-8-2]: add 'addNews' service

--- express-app/client/src/app/pages/form/form.component.ts
+++ express-app/client/src/app/pages/form/form.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsItem } from '../../modeles/news-item.model';
 import { Router } from '@angular/router';
+import { OwnnewsService } from "../../sevices/ownnews.service";
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
+
 export class FormComponent implements OnInit {
   newsItem: NewsItem[];
   formSubmitted: boolean;
 
-  constructor(private router: Router) { }
-
+  constructor(
+    private router: Router,
+    private ownnewsService: OwnnewsService
+  ) { }
 
   ngOnInit() {
     this.formSubmitted = false;
@@ -35,6 +39,10 @@ export class FormComponent implements OnInit {
 
   save() {
     console.log(this.newsItem);
+    this.ownnewsService.addNews(this.newsItem).subscribe(result => {
+      alert(`News post successfuly added.`);
+      this.router.navigate(['/']);
+    });
   }
 
   toHome() {
@@ -44,8 +52,6 @@ export class FormComponent implements OnInit {
 
   onSubmitted() {
     this.formSubmitted = true;
-    console.log('submitted', this.formSubmitted);
   }
-
 
 }
--- express-app/client/src/app/sevices/ownnews.service.ts
+++ express-app/client/src/app/sevices/ownnews.service.ts
@@ -3,16 +3,27 @@ import { Observable } from "rxjs";
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { NewsItem } from "../modeles/news-item.model";
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
 @Injectable({
   providedIn: 'root'
 })
+
 export class OwnnewsService {
   NewsItemsUrl: string = `http://localhost:4000/news/`;
 
   constructor(private http: HttpClient) { }
-  
+
   // Get News from own server
   getNewsList(): Observable<NewsItem[]> {
     return this.http.get<NewsItem[]>(this.NewsItemsUrl);
+  }
+  // Post news to own server
+  addNews(newPost: NewsItem): Observable<NewsItem[]> {
+    return this.http.post<NewsItem[]>(this.NewsItemsUrl, newPost, httpOptions);
   }
 }
